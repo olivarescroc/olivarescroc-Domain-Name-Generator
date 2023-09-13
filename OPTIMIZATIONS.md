d0b46b63280863c6068fbbd62f89fd02d47490b7
Carlos Olivares Croc <54370581+olivarescroc@users.noreply.github.com>
2023-09-13 15:15:45 +0000

    optimizations commit

---
+++ OPTIMIZATIONS.md
@@ -0,0 +1,4 @@
+Se modifica el código para que sea más fácil modificarlo agregando más pronombres, sustantivos o adjetivos.
+
+1.- se usa length en el bucle for para no tener 2 hardcoded
+2.- uso de for of para iterar sobre los arrays
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-
-
-for (let i=0; i<2; i++){
-    for (let j=0; j<2; j++){
-        for (let k=0; k<2; k++){
-           console.log( `${pronoun[i]}${adj[j]}${noun[k]}.com`);
+for (let p of pronoun) {
+    for (let a of adj) {
+        for (let n of noun) {
+            console.log(`${p}${a}${n}.com`);
         }
     }
 }
-
